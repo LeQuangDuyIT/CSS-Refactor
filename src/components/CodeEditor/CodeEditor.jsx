7087bd43ffdf7fb731fb243fc98420caed1bdd0b
lqduycp <lqduycp@gmail.com>
2023-09-27 23:40:07 +0700

    Save backup

--- src/components/CodeEditor/CodeEditor.jsx
+++ src/components/CodeEditor/CodeEditor.jsx
@@ -8,23 +8,19 @@ const CodeEditor = ({ code = '', onCodeChange, readOnly = false }) => {
 
   return (
     <>
-      <div className="basis-[5%] min-w-[48px] min-h-full">
-        <textarea
-          readOnly
-          value={orderColumnValue}
-          rows={code.split('\n').length + 1}
-          className="resize-none w-full min-h-full pl-2.5 py-4 bg-gray-400 outline-none text-center"
-        ></textarea>
-      </div>
-      <form className="flex-grow min-h-full">
-        <textarea
-          readOnly={readOnly}
-          value={code}
-          onChange={onCodeChange}
-          rows={code.split('\n').length + 1}
-          className="resize-none w-full min-h-full pl-2.5 py-4 outline-none bg-gray-300 font-mono"
-        ></textarea>
-      </form>
+      <textarea
+        readOnly
+        value={orderColumnValue}
+        rows={orderColumnValue.split('\n').length + 1}
+        className="basis-[5%] min-w-[48px] min-h-full overflow-hidden resize-none w-full pl-2.5 py-4 bg-gray-400 outline-none text-center"
+      ></textarea>
+      <textarea
+        readOnly={readOnly}
+        value={code}
+        onChange={onCodeChange}
+        rows={code.split('\n').length + 1}
+        className="flex-grow min-h-full overflow-auto resize-none w-full pl-2.5 py-4 outline-none bg-gray-300 font-mono"
+      ></textarea>
     </>
   );
 };
